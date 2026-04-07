2d843ed05671b0ff4e58e3b64b8b42c6457283c8
LuizCastr0 <silvestre.castro.luiz@gmail.com>
2026-04-07 18:20:23 -0300

    testando app.js com hello worlld, ainda com defeito

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Green Rats 🐀</Text>
+      <Text>O app está funcionando!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});

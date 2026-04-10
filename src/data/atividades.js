7e506cf2f3c648e19c8de9c49ee062f1ed14bf8c
rafael <rafaelsouzasantos111@gmail.com>
2026-04-09 22:36:37 -0300

    separando atividades e categorias em dois arquivos e complementando com novas categorias

--- src/data/atividades.js
+++ src/data/atividades.js
@@ -1,23 +1,17 @@
-// pasta dedicada a descrição e cadastro de atividades e sesus parâmetros, os dados serão puxados daqui
+// Atividades pré-carregadas aos usuários.
+// Apenas os dados fixos de cada atividade.
+// Informações de estado do usuário (favoritas, concluídas hoje, etc.) não ficam aqui (services)
 
-//cadastro de categorias de atividades
-export const categorias = [
-  { id: 'transporte', nome: 'Transporte', icone: 'car-outline' },
-  { id: 'alimentacao', nome: 'Alimentação', icone: 'fast-food-outline' },
-// preencher com mais categorias
-];
-
-
-//cadastro de atividades
 export const atividades = [
   {
-    id: 'ref-vegana',
-    categoria: 'alimentacao',
-    titulo: 'Refeição Vegana',
+    id: "ref-vegana",
+    categoria: "alimentacao",
+    titulo: "Refeição Vegana",
     pontos: 50,
-    resumo: 'Reduz o consumo de água e a emissão de metano.',
-    detalhes: 'Uma única refeição sem produtos de origem animal economiza cerca de 3.000 litros de água...',
-    linkCientifico: 'https://exemplo.com/estudo-veganismo'
+    resumo: "Reduz o consumo de água e a emissão de metano.",
+    detalhes:
+      "Uma única refeição sem produtos de origem animal economiza cerca de 3.000 litros de água...",
+    linkCientifico: "https://exemplo.com/estudo-veganismo",
   },
   //veririfcar parametros e se é necessário criar mais campos
   // preencher com mais atividades
---
+++ src/data/categorias.js
@@ -0,0 +1,34 @@
+// Categorias fixas
+
+export const categorias = [
+  {
+    id: "transporte",
+    nome: "Transporte",
+    icone: "",
+  },
+  {
+    id: "alimentacao",
+    nome: "Alimentação",
+    icone: "",
+  },
+  {
+    id: "energia",
+    nome: "Energia",
+    icone: "",
+  },
+  {
+    id: "agua",
+    nome: "Água",
+    icone: "",
+  },
+  {
+    id: "residuos",
+    nome: "Resíduos",
+    icone: "",
+  },
+  {
+    id: "consumo",
+    nome: "Consumo",
+    icone: "",
+  },
+];

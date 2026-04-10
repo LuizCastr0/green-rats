48dca668b46731722c17b58f564aeb754b1122c6
Luiz_Castr0 <luiz.castro@usp.br>
2026-04-10 15:26:47 -0300

    LogicaApp.js concentra a sequencia de funções chamadas ao completar cada atividade

---
+++ src/services/LogicaApp.js
@@ -0,0 +1,25 @@
+// scr/services/LogicaApp.js
+
+// aqui fica a lógica de negócio do app, o que acontece quando o usuário completa uma atividade,
+
+import { adicionarAcaoAoHistorico, carregarPontuacao, salvarPontuacao } from './Armazenamento';
+import { atualizarStreak } from './Armazenamento';
+
+export const registrarAtividadeCompleta = async (atividade) => {
+  try {
+    // salva no histórico e já soma os pontos automaticamente
+    await adicionarAcaoAoHistorico(atividade.titulo, atividade.pontos);
+
+    // 2. Valida o Streak (se é um novo dia, etc)
+    const novoStreak = await atualizarStreak();
+
+    return { 
+      sucesso: true, 
+      novoStreak: novoStreak.contagem,
+      // Você pode retornar mais coisas para o front fazer brilhar na tela
+    };
+  } catch (error) {
+    console.error("Falha na operação mestre:", error);
+    return { sucesso: false };
+  }
+};
